f75af5eb7eda29f0808c1a08d1fae6334b3bde01
Gonzalo <gonzalopierola@gmail.com>
2017-05-10 20:16:58 +0200

    keyboard

--- main.cpp
+++ main.cpp
@@ -4,16 +4,24 @@
 #include <math.h>
 #include <GL/gl.h>
 #include <array>
+#include <sstream>
 #include "GL/freeglut.h"
 
 
 #include "Point.h"
 
 using namespace std;
+
+static const int window_width = 600;
+static const int window_height = 600;
+
+
+bool paint = false;
 const unsigned int interval = 1000/60;
 vector<Point> puntuak;
 vector<Point> hasieraPuntuak;
 Point lastPoint;
+int iter = 0;
 
 void drawFilledCircle(GLfloat x, GLfloat y, GLfloat radius){
     int i;
@@ -33,6 +41,20 @@ void drawFilledCircle(GLfloat x, GLfloat y, GLfloat radius){
     glEnd();
 }
 
+void print(int x, int y,int z, string s)
+{
+//set the position of the text in the window using the x and y coordinates
+    glRasterPos2f(x,y);
+//get the length of the string to display
+    int len = s.size();
+
+//loop to display character by character
+    for (int i = 0; i < len; i++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12,s[i]);
+    }
+};
+
 void draw() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
@@ -40,6 +62,10 @@ void draw() {
         glColor3f(puntuak.at(i).col[0],puntuak.at(i).col[1],puntuak.at(i).col[2]);
         drawFilledCircle(puntuak.at(i).posX,puntuak.at(i).posY,2);
     }
+    ostringstream oss;
+    oss << "Iteracion: " << iter;
+    glColor3f(1.0,1.0,1.0);
+    print(5, window_height-17, 0, oss.str());
     glutSwapBuffers();
 }
 
@@ -75,7 +101,10 @@ void addPoint(){
 }
 
 void update(int value) {
-    addPoint();
+    if(paint){
+        addPoint();
+        iter++;
+    }
     glutTimerFunc(interval, update, 0);
     glutPostRedisplay();
 }
@@ -86,8 +115,32 @@ void puntuakHasieratu(){
     hasieraPuntuak.push_back(Point(290, 590,{1.0,1.0,1.0}));
 
     puntuak = hasieraPuntuak;
-    lastPoint = Point(10 + (rand() % (590 - 10 + 1)),10 + (rand() % (590 - 10 + 1)),{1.0,1.0,1.0});
-    puntuak.push_back(lastPoint);
+}
+
+void kayboard(unsigned char key, int x, int y){
+    switch (key){
+        case 13:
+            if(puntuak.size() >= 4) {
+                paint = true;
+            }
+            break;
+        case 27:
+            exit(0);
+        default:
+            break;
+    }
+}
+
+void mouse(int button, int state, int x, int y){
+    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        if(!paint){
+            if(puntuak.size() == 4){
+                puntuak.pop_back();
+            }
+            lastPoint = Point(x, window_height-y, {1.0,1.0,1.0});
+            puntuak.push_back(lastPoint);
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -97,13 +150,15 @@ int main(int argc, char **argv) {
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(600, 600);
+    glutInitWindowSize(window_width, window_height);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Chaos Game");
 
     glutDisplayFunc(draw);
+    glutKeyboardFunc(kayboard);
+    glutMouseFunc(mouse);
     glutTimerFunc(interval, update, 0);
-    enable2D(600, 600);
+    enable2D(window_width, window_height);
 
     glutMainLoop();
     return 0;
